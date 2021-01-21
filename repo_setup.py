cca6de8ca3222609812572299d2715c5fea66c96
steffesm <steffesm@users.noreply.github.com>
2021-01-21 21:11:08 +0100

    Tests: add script to add test-files
    Signed-off-by: steffesm <steffesm@users.noreply.github.com>

---
+++ repo_setup.py
@@ -0,0 +1,16 @@
+import urllib.request
+import wave
+print(urllib)
+print(wave)
+
+files = {
+    'sine1.wav': "https://www3.nd.edu/~dthain/courses/cse20211/fall2013/wavfile/sine.wav",
+    'sine2.wav': "https://www3.nd.edu/~dthain/courses/cse20211/fall2013/wavfile/sine2.wav",
+    'sine3.wav': "https://www3.nd.edu/~dthain/courses/cse20211/fall2013/wavfile/sine3.wav",
+}
+
+
+def get_files():
+    for name, link in files.items():
+        print(f"filename={name}, url={link}")
+        urllib.request.urlretrieve(link, name)
