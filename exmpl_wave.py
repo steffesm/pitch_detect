0a5282a5404ba31763431c93822ee803efdb12b5
steffesm <steffesm@users.noreply.github.com>
2021-01-21 18:55:41 +0100

    Initial commit
    Signed-off-by: steffesm <steffesm@users.noreply.github.com>

---
+++ exmpl_wave.py
@@ -0,0 +1,15 @@
+import wave
+import numpy
+
+# Read file to get buffer
+ifile = wave.open("440.wav")
+samples = ifile.getnframes()
+audio = ifile.readframes(samples)
+
+# Convert buffer to float32 using NumPy
+audio_as_np_int16 = numpy.frombuffer(audio, dtype=numpy.int16)
+audio_as_np_float32 = audio_as_np_int16.astype(numpy.float32)
+
+# Normalise float32 array so that values are between -1.0 and +1.0
+max_int16 = 2**15
+audio_normalised = audio_as_np_float32 / max_int16
