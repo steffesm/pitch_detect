85d1d45db8ebbc771a732b0fc2470fbb331c5e90
steffesm <steffesm@users.noreply.github.com>
2021-01-23 13:59:33 +0100

    test_wave_fft: exports, plt titles
    Signed-off-by: steffesm <steffesm@users.noreply.github.com>

--- test_wave_fft.py
+++ test_wave_fft.py
@@ -11,12 +11,13 @@ from wave_unify import WaveUnifyData
 plot_show = True
 plot_length = 1000
 
+# region helper functions
+
 def plot_show():
     if plot_show:
         plt.show()
 
-@pytest.fixture()
-def wav_files():
+def create_wav_files():
     file_links = {
         'sine1.wav': "https://www3.nd.edu/~dthain/courses/cse20211/fall2013/wavfile/sine.wav",
         'sine2.wav': "https://www3.nd.edu/~dthain/courses/cse20211/fall2013/wavfile/sine2.wav",
@@ -36,19 +37,46 @@ def wav_files():
             urllib.request.urlretrieve(link, name)
     return file_links.keys()
 
+def get_waves():
+    files = create_wav_files()
+    waves_dict = {}
+    for file in files:
+        wavdat = WaveUnifyData(file)
+        waves_dict.update({file: wavdat})
+    return waves_dict
+
+# endregion
+
+# region fixtures
+
+@pytest.fixture()
+def wav_files():
+    return create_wav_files()
+
+# endregion
+
+# region tests
+
 def test_plot_signals(wav_files):
     for filename in wav_files:
         fmi = WaveUnifyData(filename)
         fmi.end = plot_length
+        plot_args = {
+            'title': "Signal " + filename,
+            'normalize': True,
+        }
         if fmi.mono:
-            fmi.plot_mono(normalize=False)
+            fmi.plot_mono(**plot_args)
         else:
             signals = [
                 fmi.stereo_diff,
                 fmi.stereo_sum,
                 np.diff(signals[1]),
             ]
-            fmi.plot_signals(signals, normalize=True)
+            fmi.plot_signals(
+                signals,
+                **plot_args,
+            )
     plot_show()
 
 def test_plot_fft(wav_files):
@@ -65,6 +93,21 @@ def test_plot_fft(wav_files):
             ]
             fmi.plot_signals(signals, normalize=True)
     plot_show()
+
+# endregion
+
+# region exports
+
+files_list = list(create_wav_files())
+file1 = files_list[0]
+
+waves_dict = get_waves()
+waves_list = list(waves_dict.values())
+wave1 = waves_list[0]
+
+# endregion
+
+# region main
 
 if __name__ == '__main__':
     pytest.main()
